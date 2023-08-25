a1b421b8961e485ab5b75cacf57a016dab271e00
rishabh <rishabhrj1508@gmail.com>
2023-08-25 16:01:02 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,54 @@
+const API_KEY = "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";   
+
+const submitIcon = document.querySelector("#submit-icon");
+
+const inputElement = document.querySelector("input");
+
+const imageSection = document.querySelector('.images-section');
+
+const getImages = async () => {
+    const options = {
+        method: "POST",
+        headers: {
+            "Authorization": `Bearer ${API_KEY}`,
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            prompt: inputElement.value,
+            n: 1,
+            size: "512x512",
+        }),
+    };
+    try {
+        const response = await fetch("https://api.openai.com/v1/images/generations", options);
+        const data = await response.json();
+        
+
+        data?.data.forEach(imageObj => {
+
+            const imageContainer = document.createElement('div');
+            
+            imageContainer.classList.add('image-container');
+            
+            const imageElement = document.createElement('img');
+
+            imageElement.setAttribute('src', imageObj.url);
+
+            imageContainer.append(imageElement);
+
+            imageSection.append(imageContainer);
+            
+        });
+
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+submitIcon.addEventListener('click', getImages);
+
+
+
+
+
+
