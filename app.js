0e3ce9967359a2f8e46a8a154be6bd8e14bf4e41
rishabhrj1508 <rishabhrj1508@gmail.com>
2024-01-13 14:56:11 +0530

    first commit

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API_KEY = "";   
+const API_KEY = "your api key";
 
 const submitIcon = document.querySelector("#submit-icon");
 
@@ -6,7 +6,15 @@ const inputElement = document.querySelector("input");
 
 const imageSection = document.querySelector('.images-section');
 
+const loadingSpinner = document.querySelector('.loading-spinner');
+
 const getImages = async () => {
+
+    loadingSpinner.style.display = 'block'; // Show the loading spinner
+
+    // Clear previous images
+    imageSection.innerHTML = '';
+
     const options = {
         method: "POST",
         headers: {
@@ -22,14 +30,14 @@ const getImages = async () => {
     try {
         const response = await fetch("https://api.openai.com/v1/images/generations", options);
         const data = await response.json();
-        
+
 
         data?.data.forEach(imageObj => {
 
             const imageContainer = document.createElement('div');
-            
+
             imageContainer.classList.add('image-container');
-            
+
             const imageElement = document.createElement('img');
 
             imageElement.setAttribute('src', imageObj.url);
@@ -37,11 +45,13 @@ const getImages = async () => {
             imageContainer.append(imageElement);
 
             imageSection.append(imageContainer);
-            
+
         });
 
     } catch (error) {
         console.error(error);
+    } finally {
+        loadingSpinner.style.display = 'none'; // Hide the loading spinner
     }
 };
 
